549a1c469dd458e3b646344432867719701e18fa
lathidadia <17485874+lathidadia@users.noreply.github.com>
2022-02-23 10:49:27 -0500

    preword application complete

---
+++ Kashish_Lathidadia_Mid-Term/ByteConversion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kashish_Lathidadia_Mid_Term
+{
+    public static class ByteConversion
+    {
+        private static int Input { get; set; }
+
+        public static double MegabytesToBytes(double Input)
+        {
+            double result = Input * Math.Pow(2,20);
+            return result;
+        }
+        public static double GigabytesToMegabytes(double Input)
+        {
+            double result = Input * 1024;
+            return result;
+        }
+        public static double TerabytesToGigabytes(double Input)
+        {
+            double result = Input * 1024;
+            return result;
+        }
+
+    }
+}
---
+++ Kashish_Lathidadia_Mid-Term/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kashish_Lathidadia_Mid_Term
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                try
+                {
+                    double result = 0;
+
+                    Console.WriteLine("Enter a value to convert:");
+                    int input = int.Parse(Console.ReadLine());
+
+                    if (input < 0)
+                        throw new Exception("Please only enter numbers greater than 0.");
+
+                    Console.WriteLine("Choose your conversion:\n1. Megabytes to Bytes\n2. Gigabytes to Megabytes\n3. Terabytes to Gigabytes\n4. Exit");
+
+                    int option = int.Parse(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+                            result = ByteConversion.MegabytesToBytes(input);
+                            break;
+                        case 2:
+                            result = ByteConversion.GigabytesToMegabytes(input);
+                            break;
+                        case 3:
+                            result = ByteConversion.TerabytesToGigabytes(input);
+                            break;
+                        case 4:
+                            exit = true;
+                            break;
+                        default:
+                            throw new Exception("Incorrect option. Please try again.");
+                    }
+
+                    if (!exit)
+                        Console.WriteLine("Result: " + result.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e + "\nERROR\nPress ENTER to try again.\n");
+                    Console.ReadLine();
+                }
+            }
+            Environment.Exit(1);
+        }
+    }
+}
